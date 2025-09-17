3e61708515983fcf32be842e74668c30c8aa70d9
alngxx <anlocngdz@gmail.com>
2025-09-17 22:25:05 +0800

    reverse int

--- LeetCode/7. Reverse Integer.py
+++ LeetCode/7. Reverse Integer.py
@@ -1,7 +1,7 @@
 class Solution:
     def reverse(self, x: int) -> int:
         # Handle sign
-        sign = 1 if x > 0 else -1
+        sign = -1 if x < 0 else 1
 
         # Make x positive for easier reversing
         # Initialize res = 0: res is the reversed integer without sign
