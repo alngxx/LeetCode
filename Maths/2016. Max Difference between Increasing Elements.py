317cc29c39bd4e254ebfe3cd59688897ee7404b9
alngxx <anlocngdz@gmail.com>
2025-09-18 16:12:08 +0800

    push to Github

---
+++ Maths/2016. Max Difference between Increasing Elements.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maximumDifference(self, nums: List[int]) -> int:
+        '''
+        Intuition
+
+        Keep track of the smallest number seen so far while moving through the array.
+        At each step, we check if current number (nums[i]) >= min and nums[i] - min largest
+        If not, we update the min = nums[i]
+        '''
+
+        # Default result
+        res = -1
+
+        # Min always equal smallest number
+        min = nums[0]
+
+        for i in range(1, len(nums)):
+            if nums[i] > min:
+                res = max(res, nums[i] - min)
+            else:
+                min = nums[i]
+
+        return res
