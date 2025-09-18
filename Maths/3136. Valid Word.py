a4d69d1aa75938a1a84a798abc31da3a40fb6cd4
alngxx <anlocngdz@gmail.com>
2025-09-18 16:58:47 +0800

    push to Github

---
+++ Maths/3136. Valid Word.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValid(self, word: str) -> bool:
+        if len(word) < 3:
+            return False
+
+        vowel = 'aeiou'
+        has_vowel = False
+        has_consonant = False
+
+        for i in word:
+            if i.isalpha():
+                if i.lower() in vowel:
+                    has_vowel = True
+                else:
+                    has_consonant = True
+
+            elif not i.isdigit():
+                return False
+
+        return has_vowel and has_consonant
