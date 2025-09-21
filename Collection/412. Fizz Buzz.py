bbc51a3cea2d90fd313ed923d9bbbaa70abe77ed
alngxx <anlocngdz@gmail.com>
2025-09-22 01:14:03 +0800

    push to Github

--- Collection/412. Fizz Buzz.py
+++ Collection/412. Fizz Buzz.py
@@ -1,1 +1,19 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        ans = []
 
+        for i in range(1, n + 1):
+            FizzBuzz = (i % 15 == 0)
+            Fizz = (i % 3 == 0 and i % 5 != 0)
+            Buzz = (i % 5 == 0 and i % 3 != 0)
+
+            if Fizz:
+                ans.append("Fizz")
+            elif Buzz:
+                ans.append("Buzz")
+            elif FizzBuzz:
+                ans.append("FizzBuzz")
+            else:
+                ans.append(str(i))
+
+        return ans
