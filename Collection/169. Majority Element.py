e4c99a6d08fa4c69637541f1330d7c0a405d7249
alngxx <anlocngdz@gmail.com>
2025-09-21 17:14:43 +0800

    push to Github

--- Collection/169. Majority Element.py
+++ Collection/169. Majority Element.py
@@ -5,20 +5,20 @@ class Solution:
         nums.sort()
         return nums[n//2]
 
-        2. HashMap: {number : frequency}
+        2. HashMap: {num : freq}
         """
         n = len(nums)
-        count = {}  # Dict to store frequency/count of element
+        freq = {}  # Dict to store frequency/count of element
 
         for num in nums:
             # If number seen in dict, increase its count by 1
-            if num in count:
-                count[num] += 1
+            if num in freq:
+                freq[num] += 1
             # If number never saw before, init its count = 1
             else:
-                count[num] = 1
+                freq[num] = 1
 
         # Return the key with count > n//2
-        for key in count:
-            if count[key] > n // 2:
-                return key
+        for num in freq:
+            if freq[num] > n // 2:
+                return num
