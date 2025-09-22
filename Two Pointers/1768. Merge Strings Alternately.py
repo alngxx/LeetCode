8aa30287f88dd9985302d7c8c2193c0e6e75eb3f
alngxx <anlocngdz@gmail.com>
2025-09-23 01:08:07 +0800

    push to Github

---
+++ Two Pointers/1768. Merge Strings Alternately.py
@@ -0,0 +1,19 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        # This is quite similar to Mergesort!!
+        res = []
+        i, j = 0, 0
+
+        while i < len(word1) and j < len(word):
+            res.append(word1[i])
+            res.append(word2[j])
+            i += 1
+            j += 1
+
+        # Append leftovers (only longer word)
+        res.append(word1[i:])
+        res.append(word2[j:])
+
+        # Join the list into a string
+        string = "".join(res)
+        return string
