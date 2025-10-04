9872a60b99076fb58ecbcb83e3179f16f760399e
alngxx <anlocngdz@gmail.com>
2025-10-04 18:33:26 +0700

    push to Github

--- Linked List/19. Remove Nth Node From End of List.py
+++ Linked List/19. Remove Nth Node From End of List.py
@@ -3,22 +3,22 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        """ Brute force """
         nodes = []
         cur = head
         while cur:
             nodes.append(cur)
             cur = cur.next
+        # From now, nodes is a list stores references to each node in LL
 
-        # Index of remove node
-        remove = len(nodes) - n
-        # If remove node is head, return head.next as new head
-        if remove == 0:
+        remove_index = len(nodes) - n
+        # Special case: If removeIndex is head, return head.next as new head
+        if remove_index == 0:
             return head.next
 
         # Link previous node to next node after removed node
-        nodes[remove - 1].next = nodes[remove].next
+        nodes[remove_index - 1].next = nodes[remove_index].next
 
         return head
