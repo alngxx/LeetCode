636e74b7ecd354d883470a896fd2efb169233d41
alngxx <anlocngdz@gmail.com>
2025-10-05 16:26:13 +0700

    push to Github

---
+++ Linked List/143. Reorder List.py
@@ -0,0 +1,40 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        # Find middle (slow)
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # Reverse second halve: L(n) -> L(n-1) -> ... -> L(slow.next)
+        cur = slow.next  # Head of the second half
+        prev = slow.next = None
+        while cur:
+            temp = cur.next
+            cur.next = prev
+            prev = cur
+            cur = temp
+
+        # Link first half with second half - node by node
+        first = head  # L0
+        second = prev  # L(n)
+        while second:
+            # Save next node before link
+            temp1 = first.next
+            temp2 = second.next
+
+            # Link L0 -> L(n) -> L1 -> ...
+            first.next = second
+            second.next = temp1
+
+            # Move forwards
+            first = temp1
+            second = temp2
