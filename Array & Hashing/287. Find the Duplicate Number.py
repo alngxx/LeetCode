9fb72ce4a2a2c3f1168e4d4860bce9c63475f304
alngxx <anlocngdz@gmail.com>
2025-10-08 17:56:44 +0800

    push to Github

--- Array & Hashing/287. Find the Duplicate Number.py
+++ Array & Hashing/287. Find the Duplicate Number.py
@@ -1,6 +1,8 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
+        # New set stores element as we iterate the list
         seen = set()
+        # Iterate the list & add to set
         for num in nums:
             if num in seen:
                 return num
