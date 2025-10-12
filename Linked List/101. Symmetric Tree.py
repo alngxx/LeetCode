b6ea3659fea68bb336b5e7efb0d3ca0464c0ce39
alngxx <anlocngdz@gmail.com>
2025-10-12 17:58:32 +0800

    push to Github

---
+++ Linked List/101. Symmetric Tree.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        def isMirror(n1, n2):
+            # Base case: If both left node and right node empty -> return True
+            if not n1 and not n2:
+                return True
+            # Recursive case: Check if mirror
+            if n1 and n2 and n1.val == n2.val:
+                return isMirror(n1.left, n2.right) and isMirror(n2.left, n1.right)
+
+            return False
+
+        return isMirror(root.left, root.right)
