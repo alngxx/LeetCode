cbe1b2e9b3cb08d1099793fb1298d1a60b756b7e
alngxx <anlocngdz@gmail.com>
2025-10-16 02:36:30 +0800

    push to Github

--- Array & Hashing/1408. String Matching in an Array.py
+++ Array & Hashing/1408. String Matching in an Array.py
@@ -1,8 +1,9 @@
 def stringMatching(self, words: List[str]) -> List[str]:
-    sub_strings = set()
+    sub_strings = set()  # Don't store the same string twice when adding
     for i in words:
         for j in words:
             if i != j and i in j:
                 sub_strings.add(i)
 
+    # Convert set -> list
     return list(sub_strings)
