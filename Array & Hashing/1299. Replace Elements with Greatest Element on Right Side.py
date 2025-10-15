d736dc004bda101a15ed8d274794e45fc76d5879
alngxx <anlocngdz@gmail.com>
2025-10-15 23:37:42 +0800

    push to Github

---
+++ Array & Hashing/1299. Replace Elements with Greatest Element on Right Side.py
@@ -0,0 +1,18 @@
+class Solution:
+    def replaceElements(self, arr: List[int]) -> List[int]:
+        """
+        Intuition: O(n)
+        - Update max_right as we scan backwards as the greatest element so far on the right
+        - So we do not to scan twice to find max, which is O(n^2)
+        """
+        n = len(arr)
+        res = [-1] * n
+        max_right = -1
+
+        # Last element res[n] already -1
+        # Scan array backwards from index [n-1] to [0], update max_right as we scan
+        for i in range(n - 1, -1, -1):
+            res[i] = max_right
+            max_right = max(max_right, arr[i])
+
+        return res
