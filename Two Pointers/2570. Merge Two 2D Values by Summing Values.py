a65158715b18e180dfda4e75f4fb471ea9a8245c
alngxx <anlocngdz@gmail.com>
2025-10-16 15:02:03 +0800

    push to Github

---
+++ Two Pointers/2570. Merge Two 2D Values by Summing Values.py
@@ -0,0 +1,26 @@
+class Solution:
+    def mergeArrays(self, nums1: List[List[int]], nums2: List[List[int]]) -> List[List[int]]:
+        i = j = 0
+        res = []
+
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i][0] == nums2[j][0]:
+                id = nums1[i][0]
+                value = nums1[i][1] + nums2[j][1]
+                res.append([id, value])
+                i += 1
+                j += 1
+
+            elif nums1[i][0] < nums2[j][0]:
+                res.append(nums1[i])
+                i += 1
+
+            else:
+                res.append(nums2[j])
+                j += 1
+
+        # Extend leftovers of only one list (other list is exhausted)
+        res.extend(nums1[i:])
+        res.extend(nums2[j:])
+
+        return res
