c74609847830c1b939229792a9726e4f10a9ae9d
alngxx <anlocngdz@gmail.com>
2025-11-08 02:22:12 +0800

    push to Github

--- Dynamic Programming/509. Fibonacci Number.py
+++ Dynamic Programming/509. Fibonacci Number.py
@@ -8,7 +8,6 @@ class Solution:
         """
         if n < 2:
             return n
-
         a, b = 0, 1  # F(0) = 0; F(1) = 1
         for i in range(2, n + 1):  # F(2) = F(0) + F(1)
             a, b = b, a + b
