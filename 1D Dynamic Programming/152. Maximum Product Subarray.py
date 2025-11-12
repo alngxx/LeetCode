e0383310e530b110ff6d3f19425e05e240a2977d
alngxx <anlocngdz@gmail.com>
2025-11-12 20:29:29 +0800

    push to Github

--- 1D Dynamic Programming/152. Maximum Product Subarray.py
+++ 1D Dynamic Programming/152. Maximum Product Subarray.py
@@ -1,17 +1,22 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        res = max(nums)  # max product so far
-        cur_max = cur_min = 1  # max and min product ending at i
+        res = max(nums)  # Max product found so far. Init = max element
+        cur_max = cur_min = 1  # Keep tracks of current max and min product ending at i
 
         for i in nums:
-            # Save cur_max = previous max * current before updating
+            # Save cur_max before updating. cur_max = previous_max * i
             temp = cur_max * i
 
-            # Max is either: keep multiplying, use cur_min (if i negative), or start fresh i
+            # cur_max is either:
+            # 1. previous max * i
+            # 2. cur_min * i i.e. min negative product * i -> hence become largest
+            # 3. i i.e. start a fresh number
             cur_max = max(temp, cur_min * i, i)
 
-            # Min tracks most negative products that could become max later
+            # cur_min is similar: tracks most negative product that could become max later
             cur_min = min(temp, cur_min * i, i)
 
-            res = max(res, cur_max)  # Update max product
+            # Update res after every element i
+            res = max(res, cur_max)
+
         return res
