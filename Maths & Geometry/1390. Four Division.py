956cdba719ddcfcff8a520b197ba539668aa9805
alngxx <anlocngdz@gmail.com>
2026-01-04 17:02:19 +0700

    push to Github

--- Maths & Geometry/1390. Four Division.py
+++ Maths & Geometry/1390. Four Division.py
@@ -1,2 +1,19 @@
 class Solution:
     def sumFourDivisors(self, nums: List[int]) -> int:
+        # Function return all divisors of an integer
+        def get_divisor(n):
+            divisor = []
+            for i in range(1, int(n ** 0.5) + 1):
+                if n % i == 0:
+                    divisor.append(i)
+                    # If i is not square root of n, append it complement divisor
+                    if i != n // i:
+                        divisor.append(n // i)
+            return sorted(divisor)
+
+        # Valid int with exactly four divisors
+        total = 0
+        for num in nums:
+            if len(get_divisor(num)) == 4:
+                total += sum(get_divisor(num))
+        return total
