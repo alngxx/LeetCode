7173686aa1430f016bbe5964b43bab4df11204fa
Nguyen An Loc <anlocngdz@gmail.com>
2026-05-22 11:28:10 +0700

    560. Subarray Sum Equal K

--- Array & Hashing/560. Subarray Sum Equal K.py
+++ Array & Hashing/560. Subarray Sum Equal K.py
@@ -1,17 +1,21 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        """ Hash Map: O(n) """
-        prefix_sum = {0: 1}  # dict store frequency of prefix sum
-        total = 0  # current running sum
-        count = 0
+        """ Hash Map: O(n) 
+        Intuition: If prefix[j] - prefix[i] = k, then subarray (i, j] sums to k 
+        So for each position j, count how many previous prefix sums equal diff = total - k
+        """
+        # prefix_sum =  how many times this sum has appeared so far
+        # Edge case: handle when subarray start from index 0
+        prefix_sum = {0 : 1}  # empty prefix (sum = 0), count 1
+        total = 0    # current sum
+        count = 0 
 
         for num in nums:
-            total += num  # running sum
-            complement = total - k
+            total += num
+            diff = total - k
 
-            if complement in prefix_sum:  # check if current subarray == k, i.e. its complement already in prefix_sum
-                count += prefix_sum[complement]  # add all matching subarrays
-
-            prefix_sum[total] = prefix_sum.get(total, 0) + 1  # Update count of that prefix sum
-
+            if diff in prefix_sum:
+                count += prefix_sum[diff]     # how many subarray ending here sum = k
+            prefix_sum[total] = prefix_sum.get(total, 0) +  1  # record this prefix sum
+        
         return count
