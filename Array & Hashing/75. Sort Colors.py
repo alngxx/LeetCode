0e11c06b5353709a693a7b999cbdde890c81ad0b
Nguyen An Loc <anlocngdz@gmail.com>
2026-05-23 15:19:16 +0700

    75. Sort Colors

--- Array & Hashing/75. Sort Colors.py
+++ Array & Hashing/75. Sort Colors.py
@@ -1,22 +1,21 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         """
-        Do not return anything, modify nums in-place instead.
+        Do not return anything, modify nums in-place instead
+        Counting Sort: count each color, then overwrite the original array
+        Time complexity: O(n)
+        Space complexity: O(1) 
         """
-        red = []
-        white = []
-        blue = []
-        for i in nums:
-            if i == 0:
-                red.append(i)
-            elif i == 1:
-                white.append(i)
-            else:
-                blue.append(i)
-
-        # Concatenate three lists into one new list in order
-        res = red + white + blue
-
-        # Copy res into nums
-        for i in range(len(nums)):
-            nums[i] = res[i]
+        count = [0] * 3      # List of three store count of 0's, 1's, 2's
+        for num in nums:
+            count[num] += 1
+        
+        # Fill first count[0] with 0's
+        # Fill next count[1] with 1's
+        # Fill last count[2] with 2's
+        for i in range(count[0]):
+            nums[i] = 0
+        for j in range(count[0], len(nums) - count[2]):
+            nums[j] = 1
+        for k in range(count[0] + count[1], len(nums)):
+            nums[k] = 2
